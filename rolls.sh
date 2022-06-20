30a24f08501ac5e01e30bde01297087022c768bb
SeiNDaSz34 <80178288+SeiNDaSz34@users.noreply.github.com>
2022-06-20 11:45:43 +0100

    Update rolls.sh

--- rolls.sh
+++ rolls.sh
@@ -11,13 +11,15 @@ cd $HOME/massa/massa-client
 massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-        balance=$(./massa-client wallet_info | grep "Final rolls" | awk '{ print $3 }')
-        int_balance=${balance%%.*}
-        if [ $int_balance -le "0" ]; then
+        balance_rolls=$(./massa-client wallet_info | grep "Final rolls" | awk '{ print $3 }')
+        balance_token=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        int_balance_rolls=${balance_rolls%%.*}
+        int_balance_token=${balance_token%%.*}
+        if [[ $int_balance_rolls -le "0" ] && [ $int_balance_token -gt "99" ]]; then
                 echo "Less than 0"
                 resp=$(./massa-client buy_rolls $massa_wallet_address 1 0)
                 echo $resp
-        elif [ $int_balance -ge "1" ]; then
+        elif [ $int_balance_rolls -ge "1" ]; then
                 echo "More than 1"
         fi
         printf "sleep"
