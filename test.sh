127ac6ac308dc2d55b3001205bede7c0fe0fa719
SeiNDaSz34 <80178288+SeiNDaSz34@users.noreply.github.com>
2022-06-21 11:09:15 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
-[1 -le "0"]
-echo $?
+cd $HOME/massa/massa-client
+massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+balance_rolls=$(./massa-client wallet_info | grep "Final rolls" | awk '{ print $3 }')
+balance_token=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+int_balance_rolls=${balance_rolls%%.*}
+int_balance_token=${balance_token%%.*}
+echo $balance_rolls
+echo $balance_token
+echo $int_balance_rolls
+echo $int_balance_token
